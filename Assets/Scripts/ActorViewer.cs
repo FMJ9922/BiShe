353bb67c7431da95edd327fa7c1ddccca0d896fc
FMJ9922 <FMJ9922@users.noreply.github.com>
2020-08-23 18:06:09 +0800

    测试-根据摄像机角度切换贴图

---
+++ Assets/Scripts/ActorViewer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorViewer : MonoBehaviour
+{
+    public Sprite[] sprites;
+    Vector3 fromVector;
+    Vector3 toVector;
+    public SpriteRenderer spriteRenderer;
+
+    private void Update()
+    {
+        fromVector = PlantVector3(-transform.parent.forward);
+        toVector = PlantVector3(transform.position - Camera.main.transform.position);
+        float angle = Vector3.Angle(fromVector, toVector); //求出两向量之间的夹角
+        Vector3 normal = Vector3.Cross(fromVector, toVector);//叉乘求出法线向量
+        angle *= Mathf.Sign(Vector3.Dot(normal, Vector3.up));  //求法线向量与物体上方向向量点乘，结果为1或-1，修正旋转方向
+        transform.LookAt(Camera.main.transform.position);
+        Debug.Log(Mathf.FloorToInt(8 * (angle + 180) / 360));
+        int index = (Mathf.Clamp(Mathf.FloorToInt(8 * (angle + 180) / 360), 0, 7)+9)%8;
+        spriteRenderer.sprite = sprites[index];
+        Debug.Log(angle);
+    }
+
+    private Vector3 PlantVector3(Vector3 vector3)
+    {
+        return new Vector3(vector3.x, 0, vector3.z);
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += PlantVector3(transform.forward) * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= PlantVector3(transform.forward) * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= PlantVector3(transform.right) * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += PlantVector3(transform.right * speed);
+        }
+        /*if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -0.3f, 0), Space.World);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0.3f, 0), Space.World);
+        }*/
+        transform.LookAt(target);
+    }
+
+    private Vector3 PlantVector3(Vector3 vector3)
+    {
+        return new Vector3(vector3.x, 0, vector3.z);
+    }
+}
